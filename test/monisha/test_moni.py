cd8a018feef085496dc9f5a649bcf235700d752a
Monisha Hitang <130748910+moni2code@users.noreply.github.com>
2023-10-03 10:11:47 +0200

    created list of info for only_data

--- test/monisha/test_moni.py
+++ test/monisha/test_moni.py
@@ -8,11 +8,28 @@ URL = 'https://mobilevikings.be/en/offer/only-data/'
 
 def extract_only_data(page):
 
-    only_data = {}
+    only_data = []
     row = page.locator(f'span.callToAction__text').all_inner_texts()
-    cleaned_data = [part.split(' ')[0:4] for part in row]
+    cleaned_data = [part.split() for part in row]
     print(cleaned_data)
-
+    data_gb = cleaned_data[0][1]
+    data_price = cleaned_data[0][4].replace(',-', '')
+    
+    only_data.append({
+        'product_name': f"mobile_prepaid_only_data_{data_gb}_gb",
+        'competitor_name': 'mobile_viking',
+        'product_category': 'mobile_prepaid',
+        'product_url': URL,
+        'price': data_price,
+        'data': data_gb,
+        'minutes': "unlimited",
+        'price_per_minute': "0",
+        'sms': "no",
+        'internet_speed': ''
+    })
+    
+    print(only_data)
+    #print(row)
     return only_data
 
 def main():    
