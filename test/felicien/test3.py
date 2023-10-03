d1ad5b2aa65d1052fbb4db624ff13a15dadd2c3c
fel <felicien.dehertogh@protonmail.com>
2023-10-03 10:45:43 +0200

    add internet_speed_cleanup() function

--- test/felicien/test3.py
+++ test/felicien/test3.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import sync_playwright
 import json
+import re
 
 from pathlib import Path
 
@@ -15,25 +16,46 @@ def save_to_json(json_data, filename):
 URL = 'https://mobilevikings.be/en/offer/internet/'
 
 
+def internet_speed_cleanup(string):
+    pattern = r'(\d+)(gb|mb)'
+    gb_match = re.search(pattern, string)
+    value = int(gb_match.group(1))
+
+    if gb_match:
+        unit = gb_match.group(2)
+        if unit == "gb":
+            cleaned_value = value * 1000
+        else:
+            cleaned_value = value
+
+    return cleaned_value
+
+
 def extract_internet_data(page):
 
     internet_data = {}
 
     price = page.query_selector_all('tr.matrix__price td')[0].inner_text()
-    data = page.query_selector_all('tr.matrix__data td')[0].inner_text().lower()
-    download_speed = page.query_selector_all('tr.matrix__downloadSpeed td')[0].inner_text().encode('ascii', 'ignore').decode('ascii').lower()
-    upload_speed = page.query_selector_all('tr.matrix__voice td')[0].inner_text().encode('ascii', 'ignore').decode('ascii').lower()
+    cleaned_price = re.sub(r'\D', '', price)
+
+    monthly_data = page.query_selector_all('tr.matrix__data td')[0].inner_text().lower()
+
+    download_speed = page.query_selector_all('tr.matrix__downloadSpeed td')[0].inner_text().encode('ascii', 'ignore').decode('ascii').lower().strip()
+    upload_speed = page.query_selector_all('tr.matrix__voice td')[0].inner_text().encode('ascii', 'ignore').decode('ascii').lower().strip()
+
+    cleaned_download_speed = internet_speed_cleanup(download_speed)
+    cleaned_upload_speed = internet_speed_cleanup(upload_speed)
 
     internet_data['competitor_name'] = 'mobile_viking'
     internet_data['product_category'] = 'internet_subscription'
     internet_data['product_url'] = URL
-    internet_data['price'] = price
-    internet_data['data'] = data
+    internet_data['price'] = cleaned_price
+    internet_data['data'] = monthly_data
     internet_data['network'] = ''
     internet_data['minutes'] = ''
     internet_data['sms'] = ''
-    internet_data['download_speed'] = download_speed
-    internet_data['upload_speed'] = upload_speed
+    internet_data['download_speed'] = cleaned_download_speed
+    internet_data['upload_speed'] = cleaned_upload_speed
 
     return internet_data
 
