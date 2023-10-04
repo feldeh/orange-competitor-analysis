7a77cd587ecffbb557616c2684202933cf192377
fel <felicien.dehertogh@protonmail.com>
2023-10-05 00:17:48 +0200

    add generate_packs function to viking scraper

--- scraper/mobileviking/utils.py
+++ scraper/mobileviking/utils.py
@@ -1,19 +1,25 @@
 from pathlib import Path
 import ndjson
 import json
+import logging
 
 
 def save_to_json(dict_data, filename):
-    json_path = Path.cwd() / f"data/raw_data/json/{filename}.json"
+    json_path = f"data/raw_data/json/{filename}.json"
     json_data = json.dumps(dict_data, indent=4)
-    with json_path.open(mode="w", encoding="utf-8") as f:
+    with open(json_path, mode="w", encoding="utf-8") as f:
         f.write(json_data)
 
-    print(f"JSON data saved to {json_path}")
+    file_saved_message = f"{filename} JSON file saved | {json_path}"
+    print(file_saved_message)
+    logging.info(file_saved_message)
 
 
 def save_to_ndjson(list_data, filename):
-    ndjson_path = Path.cwd() / f"data/raw_data/ndjson/{filename}.ndjson"
-    with ndjson_path.open(mode="w", encoding="utf-8") as f:
+    ndjson_path = f"data/raw_data/ndjson/{filename}.ndjson"
+    with open(ndjson_path, mode="w", encoding="utf-8") as f:
         ndjson.dump(list_data, f)
-    print(f"NDJSON data saved to {ndjson_path}")
+
+    file_saved_message = f"{filename} NDJSON file saved | {ndjson_path}"
+    print(file_saved_message)
+    logging.info(file_saved_message)
