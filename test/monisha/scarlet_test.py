803e26d895a66ad1c95294cdcdfa689f6235b665
Monisha Hitang <130748910+moni2code@users.noreply.github.com>
2023-10-05 16:49:02 +0200

    scraped mobile_subscription

--- test/monisha/scarlet_test.py
+++ test/monisha/scarlet_test.py
@@ -19,25 +19,37 @@ def extract_mobile_subscription_data(page_content, url):
 
     mobile_subscription_data = []
     soup = BeautifulSoup(page_content, 'html.parser')
-    mobile_subscription_elements = soup.find_all('div', class_='rs-ctable-panel')
-    for element in mobile_subscription_elements:
-        
+    mobile_subscription_elements = soup.find_all('div', class_="rs-ctable-panel jsrs-resizerContainer")
+    #print(mobile_subscription_elements)
     
-    mobile_subscription_data.append({
-                'product_name': f"mobile_subscription_{mobile_data}_gb",
-                'competitor_name': 'scarlet',
-                'product_category': 'mobile_subscription',
-                'product_url': url,
-                'price': price_per_month,
-                'data': mobile_data,
-                'network': network,
-                'minutes': minutes,
-                'price_per_minute': '',
-                'sms': sms,
-                'upload_speed': '',
-                'download_speed': '',
-                'line_type': ''
-            })
+    for element in mobile_subscription_elements:
+        product_name = element.find('h3', class_="rs-ctable-panel-title").get_text().strip()
+        price_per_month = element.find('span', class_="rs-unit").get_text()
+        elms = element.find_all('li', class_="jsrs-resizerPart")
+        mobile_data = elms[0].get_text()
+        minutes = elms[2].get_text()
+        sms = elms[1].get_text()
+        '''for l in elms:
+            print(l.get_text())'''
+        
+            
+    
+        mobile_subscription_data.append({
+            'product_name': f"mobile_subscription_{product_name}",
+            'competitor_name': 'scarlet',
+            'product_category': 'mobile_subscription',
+            'product_url': url,
+            'price': price_per_month,
+            'data': mobile_data,
+            'network': "unknown",
+            'minutes': minutes,
+            'price_per_minute': '',
+            'sms': sms,
+            'upload_speed': '',
+            'download_speed': '',
+            'line_type': ''
+                })
+       # print(element.find('li', class_="jsrs-resizerPart").get_text())
     return mobile_subscription_data
   
 def get_mobile_subscription_data(browser, url):
