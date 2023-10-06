32e7060316cae72e90294a6e327e2cdbc04040ff
fel <felicien.dehertogh@protonmail.com>
2023-10-06 09:37:44 +0200

    add wait_for_file task to load_to_bigquery dag

--- dags/load_to_bigquery_dag.py
+++ dags/load_to_bigquery_dag.py
@@ -1,10 +1,15 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.sensors.python import PythonSensor
+from airflow.sensors.time_delta import TimeDeltaSensor
 
 from bigquery import load_json_to_bigquery
 from google.cloud import bigquery
 import os
+import time
+from pathlib import Path
+
 
 service_acc_key_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
@@ -19,6 +24,20 @@ DEFAULT_DAG_ARGS = {
     'retry_delay': timedelta(minutes=1)
 }
 
+
+def check_ndjson_exist(file_names):
+    while True:
+        all_exist = True
+        for file_name in file_names:
+            file_path = Path.cwd() / 'data' / 'raw_data' / 'ndjson' / f'{file_name}.ndjson'
+            if not file_path.is_file():
+                all_exist = False
+                break
+        if all_exist:
+            return True
+        else:
+            time.sleep(5)
+
 with DAG(
 
     default_args=DEFAULT_DAG_ARGS,
@@ -30,6 +49,14 @@ with DAG(
 
 ) as dag:
 
+
+    wait_for_file = PythonSensor(
+        task_id='wait_for_file',
+        python_callable=check_ndjson_exist,
+        op_kwargs={"file_names": TABLE_NAMES},
+
+    )
+
     load_to_bigquery = PythonOperator(
         task_id='load_to_bigquery',
         python_callable=load_json_to_bigquery,
@@ -37,4 +64,4 @@ with DAG(
 
     )
 
-    load_to_bigquery
+    wait_for_file >> load_to_bigquery
